e7e4bce7c4123ab75dde191117eb1e2a1ef38c4f
Scott Deeter <deetergp@gmail.com>
2011-11-17 17:31:40 -0500

    added logic to discern between date and time

--- script.php
+++ script.php
@@ -1,3 +1,16 @@
 <?php
-echo date('l jS \of F Y');
+// Set your default time zone
+date_default_timezone_set('America/New_York');
+
+$format = 'x';
+
+if ($format == 'd') {
+  echo date('l jS \of F Y');
+}
+else if ($format == 't') {
+  echo date('h\:i\:s A');
+}
+else {
+  echo date('l jS \of F Y');
+}
 ?>
