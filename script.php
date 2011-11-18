e0d5fc35fc54f091aa3cfa7b1d8f9f6999abf312
Scott Deeter <deetergp@gmail.com>
2011-11-17 23:41:25 -0500

    Included ability to select date or time

--- script.php
+++ script.php
@@ -2,13 +2,16 @@
 // Set your default time zone
 date_default_timezone_set('America/New_York');
 
-$format = 'd';
+$q=$argv[1];
 
-if ($format == 'd') {
+if ($q == 'd') {
   echo date('l\, F jS\, Y');
 }
-else if ($format == 't') {
+else if ($q == 't') {
   echo date('h\:i\:s A');
+}
+else if ($q == 'b') {
+  echo date('l\, F jS\, Y h\:i\:s A');
 }
 else {
   echo date('l jS \of F Y');
